2929d34367503e2259eba1e511e72681f4f49504
kaustuv deolal <vutsuak96@gmail.com>
2016-11-13 14:48:48 +0530

    material added

---
+++ python_basics/factorial.py
@@ -0,0 +1,2 @@
+x=int(raw_input("enter a number"))
+print reduce(lambda x,y : x*y ,range(1,6)) 
---
+++ python_basics/list_comprehension.py
@@ -0,0 +1,3 @@
+l= [str(i)+"even" if i %2 ==0 else str(i)+"odd" for i in range(6) ]
+for i in l:
+	print i
